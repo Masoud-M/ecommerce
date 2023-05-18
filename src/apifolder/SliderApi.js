0b496c2ef2c79035bdd0d4e797e5962a074e6d28
Masoud-M <48066903+Masoud-M@users.noreply.github.com>
2023-05-18 11:48:29 +0330

    Update SliderApi.js

--- src/apifolder/SliderApi.js
+++ src/apifolder/SliderApi.js
@@ -5,14 +5,22 @@ export const ApiSlides = [
       h2: "Hey this is an awsome deal today",
       p: "Up to 40% off",
     },
-    background: "bg-[#ffd028]",
+    background: "bg-[#ed9b99]",
   },
   {
-    src: "https://img.freepik.com/free-photo/blithesome-slim-girl-french-beret-dancing-winsome-short-haired-lady-black-skirt-jumping-rosy-wall_197531-20382.jpg",
+    src: "https://img.freepik.com/free-photo/studio-shot-pretty-black-woman-with-white-shopping-bag-standing-yellow-background-trendy-spring-fashionable-look_273443-10.jpg?w=996&t=st=1684397667~exp=1684398267~hmac=0aa525fa5ece00c8979619d0ce699645b2402fee4c0fcfba97ea6b7e4f7aec1b",
     content: {
       h2: "Hey this is an awsome deal today",
       p: "Up to 40% off",
     },
-    background: "bg-[#ffd028]",
+    background: "bg-[#f3a916]",
+  },
+  {
+    src: "https://img.freepik.com/free-photo/graceful-african-model-posing-pink-fashionable-jacket-white-shorts-yellow-background-studio_273443-62.jpg?w=1380&t=st=1684397787~exp=1684398387~hmac=4807308bc9bcfa258b9f6d07ffb4f58446ce2588a65d4d0fafc0003eca29239b",
+    content: {
+      h2: "Hey this is an awsome deal today",
+      p: "Up to 40% off",
+    },
+    background: "bg-[#f3aa0f]",
   },
 ];
