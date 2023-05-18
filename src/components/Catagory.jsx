2c8f27c972023b5d6b9dc5fe709004712a13bdaf
Masoud-M <48066903+Masoud-M@users.noreply.github.com>
2023-05-18 14:13:04 +0330

    Update Catagory.jsx

--- src/components/Catagory.jsx
+++ src/components/Catagory.jsx
@@ -5,7 +5,7 @@ function Catagory({ item }) {
   return (
     <div className="flex-1 m-2 shadow-lg rounded-md overflow-hidden relative">
       <img className="w-full " src={item.src} alt="/" />
-      <div className="flex absolute w-full h-full left-0 top-0 items-center justify-center flex-col">
+      <div className=" hover:bg-black/30 transition flex absolute w-full h-full left-0 top-0 items-center justify-center flex-col">
         <h2 className="text-white font-medium text-[30px]">{item.title}</h2>
         <button className="btn">
           <Link to="/category">See more</Link>
